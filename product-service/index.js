3c66d8efb9ad77f03eb0fd30e0b076ff9e3dda9c
rishabh <sainirishabh394@gmail.com>
2026-03-05 12:05:32 +0530

    improved the project

--- product-service/index.js
+++ product-service/index.js
@@ -1,10 +1,46 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Product Service Running 🚀");
-});
+app.get("/", (req,res)=>{
 
-app.listen(4000, () => {
-  console.log("Product Service running on port 4000");
-});
+res.send(`
+<html>
+<head>
+<title>Product Service</title>
+
+<style>
+body{
+font-family: Arial;
+background: linear-gradient(135deg,#11998e,#38ef7d);
+text-align:center;
+color:white;
+padding-top:100px;
+}
+
+.box{
+background:rgba(0,0,0,0.2);
+padding:40px;
+width:400px;
+margin:auto;
+border-radius:15px;
+}
+
+</style>
+</head>
+
+<body>
+
+<div class="box">
+<h1>Product Service</h1>
+<p>Docker Microservice Running</p>
+<p>DevOps Pipeline Ready</p>
+</div>
+
+</body>
+</html>
+`)
+})
+
+app.listen(5002,()=>{
+console.log("Product service running on 5002")
+})
--- user-service/index.js
+++ user-service/index.js
@@ -1,10 +1,41 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
 
 app.get("/", (req, res) => {
-  res.send("User Service Running 🚀");
-});
+res.send(`
+<html>
+<head>
+<title>User Service</title>
+<style>
+body{
+font-family: Arial;
+background: linear-gradient(135deg,#667eea,#764ba2);
+color:white;
+text-align:center;
+padding-top:100px;
+}
+.card{
+background:rgba(255,255,255,0.1);
+padding:40px;
+border-radius:15px;
+width:400px;
+margin:auto;
+box-shadow:0 10px 20px rgba(0,0,0,0.3);
+}
+</style>
+</head>
 
-app.listen(3000, () => {
-  console.log("User service running on port 3000");
-});
+<body>
+<div class="card">
+<h1>User Service</h1>
+<p>Microservice Running Successfully</p>
+<p>CI/CD Ready</p>
+</div>
+</body>
+</html>
+`)
+})
+
+app.listen(5001,()=>{
+console.log("User Service running on 5001")
+})
